2ebef4779bb95f55c46c8398b74f2594b87d6e6e
Selva1507 <selvakumarnamakodi@gmail.com>
2025-08-22 10:50:24 +0530

    V1

--- checker.cs
+++ checker.cs
@@ -3,46 +3,83 @@ using System.Diagnostics;
 
 class Checker
 {
-    public static bool VitalsOk(float temperature, int pulseRate, int spo2)
+    const float MIN_TEMPERATURE = 95.0f;
+    const float MAX_TEMPERATURE = 102.0f;
+    const int MIN_PULSE = 60;
+    const int MAX_PULSE = 100;
+    const int MIN_SPO2 = 90;
+
+    public static bool CheckTemperature(float temperature)
     {
-        if(temperature >102 || temperature < 95)
+        if (temperature > MAX_TEMPERATURE || temperature < MIN_TEMPERATURE)
         {
-            Console.WriteLine("Temperature critical!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            Console.WriteLine("Temperature is critical!");
+            BlinkAlert();
             return false;
         }
-        else if (pulseRate < 60 || pulseRate > 100)
+        else
         {
-            Console.WriteLine("Pulse Rate is out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
-            return false;
+            Console.WriteLine("Temperature is normal");
+            return true;
         }
-        else if (spo2 < 90)
-        {
-            Console.WriteLine("Oxygen Saturation out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
-            return false;
-        }
-        Console.WriteLine("Vitals received within normal range");
-        Console.WriteLine("Temperature: {0} Pulse: {1}, SO2: {2}", temperature, pulseRate, spo2);
-        return true;
     }
-}
+
+    public static bool CheckPulseRate(int pulseRate)
+    {
+        if (pulseRate < MIN_PULSE || pulseRate > MAX_PULSE)
+        {
+            Console.WriteLine("Pulse Rate is critical!");
+            BlinkAlert();
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Pulse Rate is normal");
+            return true;
+        }
+    }
+
+    public static bool CheckOxygenSaturation(int spo2)
+    {
+        if (spo2 < MIN_SPO2)
+        {
+            Console.WriteLine("Oxygen Saturation is critical!");
+            BlinkAlert();
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Oxygen Saturation is normal");
+            return true;
+        }
+    }
+    
+    public static bool IsAllVitals_OK(float temperature, int pulseRate, int spo2)
+    {
+        bool IsTemperature_OK  = CheckTemperature(temperature);
+        bool IsPulseRate_OK    = CheckPulseRate(pulseRate);
+        bool IsSpo2_OK         = CheckOxygenSaturation(spo2);
+        
+        if (IsTemperature_OK && IsPulseRate_OK && IsSpo2_OK)
+        {
+            Console.WriteLine("Result: All vitals are within normal range.\n");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Result: One or more vitals are critical!\n");
+            return false;
+        }
+    }
+
+    private static void BlinkAlert()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("\r* ");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("\r *");
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine("\n");
+    }
